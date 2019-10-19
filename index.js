a248df76a36aaef3dae8379408305c8c2280058e
LAYTHJABBAR <layth.jabbar@yahoo.com>
2019-10-19 22:19:01 +0000

    array

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let sum = 0;
+for (let item in array){
+    if (Array.isArray(array[item]) === false) {
+     sum += array[item]
+} else {
+  sum += sumItems(array[item])
+} 
+} return sum;
 }
 
 module.exports = sumItems;
+
+
+console.log(sumItems([1, 2, 3, 4, 5]));
+console.log(sumItems([[1, 2, [[3], 4]], 5, []]))
+console.log(sumItems([[[[[[[[[[[[[1]]]]]]]]]]]]]))
